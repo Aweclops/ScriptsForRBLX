ac2a9dd6f8c204e6544a23d4c7bc3ef14c2ee6ed
Aweclops <47144650+Aweclops@users.noreply.github.com>
2020-05-28 14:49:59 +0530

    TeleportTool
    yeet

---
+++ TeleportTool.lua
@@ -0,0 +1,17 @@
+local tool = Instance.new("Tool")
+tool.Name = "Teleport(Click E)"
+local tclone = tool:Clone()
+tclone.Parent = game.Players.LocalPlayer
+
+local script = Instance.new("LocalScript", tclone)
+local code = Instance.new("Stringvalue",script)
+code.Value = "
+local tool = script.Parent
+tool.Button1Down:Connect(function()
+	local plr = game.Players.LocalPlayer
+	local mouse = plr:GetMouse()
+	local char = plr.Character
+	char:moveTo(mouse.Target)
+end
+"
+loadstring(code.Value)()
